b1ef3582a922fa5806d86617cccce60ddfd8fb26
Mike <mike-mo@users.noreply.github.com>
2023-09-10 16:32:10 -0700

    Generate readable emails

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -2,24 +2,43 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   'use strict';
   const fakeEmails = {};
 
-  alert("Show all properties of request: " + JSON.stringify(request));
+  //alert("Show all properties of request: " + JSON.stringify(request));
 
-  function replaceWord() {
-    // Find all occurrences of "no" in the body and replace them with "yes"
-    $("body").each(function () {
-      var content = $(this).html();
-      // Replace "no" (case insensitive) with yes
-      var newContent = content.replace(/NO/gi, "YES");
-      $(this).html(newContent);
-    });
+  function generateRandomWord(length) {
+    const vowels = 'aeiou';
+    // declare a string of consonants where each is repeated proportionally to its frequency in English
+    const consonants = 'bbcccdddfffgghhhjjkklllmmmnnnppqrrrrsssssttttttvvwwxyyz';
+    let word = '';
+
+    for (let i = 0; i < length; i++) {
+      if (i % 2 === 0) {
+        // Add a consonant for even positions
+        word += consonants.charAt(Math.floor(Math.random() * consonants.length));
+      } else {
+        // Add a vowel for odd positions
+        word += vowels.charAt(Math.floor(Math.random() * vowels.length));
+      }
+    }
+    return word;
   }
-  replaceWord();
+
+
+  // function replaceWord() {
+  //   // Find all occurrences of "no" in the body and replace them with "yes"
+  //   $("body").each(function () {
+  //     var content = $(this).html();
+  //     // Replace "no" (case insensitive) with yes
+  //     var newContent = content.replace(/NO/gi, "YES");
+  //     $(this).html(newContent);
+  //   });
+  // }
+  // replaceWord();
 
   function getFakeEmail(baseEmail) {
     const domain = 'example.com'; // Choose your fake domain
     if (!fakeEmails[baseEmail]) {
-      const randomPart = Math.random().toString(36).substring(2, 10); // Generate a random string
-      fakeEmails[baseEmail] = `fake_${randomPart}@${domain}`;
+      const randomPart = generateRandomWord(Math.floor(4 + Math.random() * 4)); // Generate a random string
+      fakeEmails[baseEmail] = `${randomPart}*@${domain}`;
     }
     return fakeEmails[baseEmail];
   }
